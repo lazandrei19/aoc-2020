3258dd7b5934e2a7cf8f2bcc69b35e609d934ac4
Andrei Lazarescu <lazarescu.andrei19@gmail.com>
2020-12-02 00:49:36 +0200

    Day 1

---
+++ day1/day1.py
@@ -0,0 +1,25 @@
+from os import path
+
+dir_path = path.dirname(path.realpath(__file__))
+
+f = open(path.join(dir_path, "input.txt"), "r")
+number_set = set(map(int, f.readlines()))
+
+
+def find_pair_with_sum(sum):
+    for number in number_set:
+        complement = sum - number
+        if complement in number_set:
+            return (number, complement)
+    return None
+
+
+# ex 1
+(a, b) = find_pair_with_sum(2020)
+print(a * b)
+
+for number in number_set:
+    pair = find_pair_with_sum(2020 - number)
+    if pair is not None:
+        print(pair[0] * pair[1] * number)
+        exit(0)
